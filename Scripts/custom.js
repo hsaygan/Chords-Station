8f9738c91ef074fd56f8314b60035e0d355c69cd
hsaygan <realyashnag@gmail.com>
2018-04-17 06:46:32 +0530

    working "Updating Chords" feature

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,13 +1,54 @@
 var database_link = "https://hsaygan.github.io/Chords-Station/database.json";
 
-function run_this_random(song_name, artist_name)
+function postUpdatedChord(timer, time_interval, chord)
 {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function()
+    setTimeout(function() {
+        console.log("\t\tCurrent Time: ", timer.getTimeValues().minutes, ":", timer.getTimeValues().seconds, ":", timer.getTimeValues().secondTenths);
+        document.getElementById("display_chord").innerHTML = chord;
+    }, time_interval);
+}
+
+function getSongChords(database, file_object)
+{
+    console.log("\n=========== LEVEL 2");
+
+    //Initializations
+    //document.getElementById('calendar').src = database[artist_name][i].youtube_link;
+    var timer = new Timer();
+    timer.start();
+
+    //Display Timer
+    timer.addEventListener('secondsUpdated', function (e) {
+        document.getElementById("timer").innerHTML = timer.getTimeValues().toString();
+    });
+
+    //Display Chords
+    var all_lines = file_object.toString().split("\n");
+    console.log("\nAll Lines: ",  all_lines);
+    for (var j = 0; j < all_lines.length-1; j++)
+    {
+        var current_line = all_lines[j].split(":");
+        console.log("\nCurrent Line: " + current_line);
+        var minutes = parseInt(current_line[0]);
+        var seconds = parseInt(current_line[1]);
+        var secondTenths = parseInt(current_line[2]);
+        var chord = current_line[3];
+        var time_interval = Math.abs(60*1000*(timer.getTimeValues().minutes-minutes)) + Math.abs(1000*(timer.getTimeValues().seconds-seconds)) + Math.abs(100*(timer.getTimeValues().secondTenths-secondTenths)) ;
+        console.log("\n\tTime Interval: ", time_interval);
+        console.log("\n\tChange Time: ", minutes, ":", seconds, ":", secondTenths, " to Chord: ", chord);
+
+        postUpdatedChord(timer, time_interval, chord);
+    }
+}
+
+function findSongChords(song_name, artist_name)
+{
+    var xhr1 = new XMLHttpRequest();
+    xhr1.onreadystatechange = function()
     {
         if (this.readyState == 4 && this.status == 200)
         {
-            var database = JSON.parse(xhttp.responseText);
+            var database = JSON.parse(xhr1.responseText);
             console.log("\n=========== LEVEL 1\n Database = ", database);
 
             var song_found = false;
@@ -21,79 +62,17 @@ function run_this_random(song_name, artist_name)
                                 "\n\tLink: " + database[artist_name][i].chords_link +
                                 "\n\tYoutube Link: " + database[artist_name][i].youtube_link);
 
-                    var xhr = new XMLHttpRequest();
-                    xhr.onreadystatechange = function()
+                    var xhr2 = new XMLHttpRequest();
+                    xhr2.onreadystatechange = function()
                     {
                         if (this.readyState == 4 && this.status == 200)
                         {
-                            var file_object = xhr.responseText;
-                            console.log("\n=========== LEVEL 1\n File Object = ", file_object);
-
-                            //Initializations
-                            //document.getElementById("youtube_video").src = database[artist_name][i].youtube_link;
-                            var timer = new Timer();
-                            timer.start();
-
-                            //Display Timer
-                            timer.addEventListener('secondsUpdated', function (e) {
-                                document.getElementById("timer").innerHTML = timer.getTimeValues().toString();
-                            });
-
-                            //Display Chords
-                            var all_lines = file_object.toString().split("\n");
-                            console.log("\nAll Lines: ",  all_lines);
-                            for (var j = 0; j < all_lines.length-1; j++)
-                            {
-                                current_line = all_lines[j].split(":");
-                                console.log("\nCurrent Line: " + current_line);
-                                var minutes = parseInt(current_line[0]);
-                                var seconds = parseInt(current_line[1]);
-                                var secondTenths = parseInt(current_line[2]);
-                                var chord = current_line[3];
-                                var time_interval = Math.abs(60*1000*(timer.getTimeValues().minutes-minutes)) + Math.abs(1000*(timer.getTimeValues().seconds-seconds)) + Math.abs(100*(timer.getTimeValues().secondTenths-secondTenths)) ;
-                                console.log("\n\tTime Interval: ", time_interval);
-                                console.log("\n\tChange Time: ", minutes, ":", seconds, ":", secondTenths, " to Chord: ", chord);
-
-                                var passed_change_time = false;
-                                setTimeout(function() {
-                                    console.log("\t\tCurrent Time: ", timer.getTimeValues().minutes, ":", timer.getTimeValues().seconds, ":", timer.getTimeValues().secondTenths);
-                                    document.getElementById("display_chord").innerHTML = chord;
-                                }, time_interval);
-
-                                /*setTimeout(function(){
-                                    console.log("Current Time: ", timer.getTimeValues().hours, ":", timer.getTimeValues().minutes, ":", timer.getTimeValues().seconds);
-                                    if (hours == timer.getTimeValues().hours)
-                                    {
-                                        if (minutes == timer.getTimeValues().minutes)
-                                        {
-                                            if (seconds == timer.getTimeValues().seconds)
-                                            {
-                                                document.getElementById("display_chord").innerHTML = chord;
-                                            }
-                                            else if (seconds < timer.getTimeValues().seconds)
-                                            {
-                                                passed_change_time = true;
-                                            }
-                                        }
-                                        else if (minutes < timer.getTimeValues().minutes)
-                                        {
-                                            passed_change_time = true;
-                                        }
-                                    }
-                                    else if (hours < timer.getTimeValues().hours)
-                                    {
-                                        passed_change_time = true;
-                                    }
-                                    if (passed_change_time == true)
-                                    {
-                                        break;
-                                    }
-                                }, 1000);*/
-                            }
+                            var file_object = xhr2.responseText;
+                            getSongChords(database, file_object);
                         }
                     };
-                    xhr.open("GET", database[artist_name][i].chords_link, true);
-                    xhr.send();
+                    xhr2.open("GET", database[artist_name][i].chords_link, true);
+                    xhr2.send();
                 }
             }
             if (song_found == false)
@@ -102,10 +81,8 @@ function run_this_random(song_name, artist_name)
             }
         }
     };
-    xhttp.open("GET", database_link, true);
-    xhttp.send();
+    xhr1.open("GET", database_link, true);
+    xhr1.send();
 }
-    /*//Testing
-    */
 
-run_this_random("Dilnawaaz", "Local Train")
+findSongChords("Dil Mere", "Local Train")
